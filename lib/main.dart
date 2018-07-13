8f833e09332607a0b86883bfa42cead9ff97d46e
NITISH KUMAR SINGH <nitishk72@gmail.com>
2018-07-14 04:43:04 +0530

    foreground notification fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'dart:async';
 
 void main() => runApp(new MyApp());
 
@@ -12,16 +14,30 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   String textValue = 'Hello World !';
   FirebaseMessaging firebaseMessaging = new FirebaseMessaging();
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      new FlutterLocalNotificationsPlugin();
 
   @override
   void initState() {
-    firebaseMessaging.configure(onLaunch: (Map<String, dynamic> msg) {
-      print(" onLaunch called");
-    }, onResume: (Map<String, dynamic> msg) {
-      print(" onResume called");
-    }, onMessage: (Map<String, dynamic> msg) {
-      print(" onMessage called");
-    });
+    super.initState();
+
+    var android = new AndroidInitializationSettings('mipmap/ic_launcher');
+    var ios = new IOSInitializationSettings();
+    var platform = new InitializationSettings(android, ios);
+    flutterLocalNotificationsPlugin.initialize(platform);
+
+    firebaseMessaging.configure(
+      onLaunch: (Map<String, dynamic> msg) {
+        print(" onLaunch called ${(msg)}");
+      },
+      onResume: (Map<String, dynamic> msg) {
+        print(" onResume called ${(msg)}");
+      },
+      onMessage: (Map<String, dynamic> msg) {
+        showNotification(msg);
+        print(" onMessage called ${(msg)}");
+      },
+    );
     firebaseMessaging.requestNotificationPermissions(
         const IosNotificationSettings(sound: true, alert: true, badge: true));
     firebaseMessaging.onIosSettingsRegistered
@@ -33,10 +49,22 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
+  showNotification(Map<String, dynamic> msg) async {
+    var android = new AndroidNotificationDetails(
+      'sdffds dsffds',
+      "CHANNLE NAME",
+      "channelDescription",
+    );
+    var iOS = new IOSNotificationDetails();
+    var platform = new NotificationDetails(android, iOS);
+    await flutterLocalNotificationsPlugin.show(
+        0, "This is title", "this is demo", platform);
+  }
+
   update(String token) {
     print(token);
     DatabaseReference databaseReference = new FirebaseDatabase().reference();
-    databaseReference.child('fcm-token/${token}').set({"token":token});
+    databaseReference.child('fcm-token/${token}').set({"token": token});
     textValue = token;
     setState(() {});
   }
@@ -49,8 +77,12 @@ class _MyAppState extends State<MyApp> {
           title: new Text('Push Notification'),
         ),
         body: new Center(
-          child: new Text(
-            textValue,
+          child: new Column(
+            children: <Widget>[
+              new Text(
+                textValue,
+              )
+            ],
           ),
         ),
       ),
